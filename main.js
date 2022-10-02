2a075c4c8a62c17b91c3b7db7ad7904f055136b7
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-10-02 21:26:28 +0200

    working

---
+++ main.js
@@ -0,0 +1,62 @@
+import { clients, refreshClients } from "../../private/clients.js";
+
+export class App {
+  client;
+  customFunctions = [];
+
+  constructor(config) {
+    this.config = config;
+    this.findClient();
+  }
+
+  async updateDefinitions() {
+    if (!this.client) this.findClient();
+    if (!this.client) return;
+    let result = await this.client.request("http", "request", [
+      "POST",
+      this.config.url,
+      JSON.stringify({
+        arguments: [this.config.pwd],
+        export: "serviceStatus",
+        module: "public/service.js",
+      }),
+      "application/json",
+    ]);
+    for (let functionName in this.definitions.methods)
+      delete this[functionName];
+
+    if (!result.result.success) return;
+
+    let services = JSON.parse(result.result.response).data;
+    this.updateDefinitions.methods = {};
+
+    for (let service of services) {
+      this.definitions.methods[service.service] = {
+        arguments: [],
+        name:
+          service.service +
+          " is " +
+          (service.active ? "active" : "not active") +
+          ". Change active status?",
+      };
+
+      this[service.service] = async () => {
+        await this.client.request("http", "request", [
+          "POST",
+          this.config.url,
+          JSON.stringify({
+            arguments: [this.config.pwd, service.service],
+            export: "changeServiceStatus",
+            module: "public/service.js",
+          }),
+          "application/json",
+        ]);
+      };
+    }
+  }
+
+  async findClient() {
+    await refreshClients();
+    if (clients[this.config.client]) this.client = clients[this.config.client];
+  }
+}
